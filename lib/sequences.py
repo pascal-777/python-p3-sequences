aa14afbd8cdf9dfb106b97005fb3f78bb704a87d
pascal <pascal.nicintije@student.moringaschool.com>
2024-05-27 09:38:14 +0300

    completes lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+    if length <= 0:
+        print(fibonacci_sequence)
+        return fibonacci_sequence
+    elif length == 1:
+        fibonacci_sequence.append(0)
+    elif length == 2:
+        fibonacci_sequence.extend([0, 1])
+    else:
+        fibonacci_sequence.extend([0, 1])
+        while len(fibonacci_sequence) < length:
+            next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(next_number)
+    print(fibonacci_sequence)
